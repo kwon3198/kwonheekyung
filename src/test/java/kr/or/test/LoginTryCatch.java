b20e77f3b0863e993fe54a9a0b0f8fb67bd6bacd
Administrator <Administrator@WIN-JCT78BHHOI0>
2020-06-12 09:05:58 +0900

    LoginTryCatch 수정완료

--- src/test/java/kr/or/test/LoginTryCatch.java
+++ src/test/java/kr/or/test/LoginTryCatch.java
@@ -1,7 +1,7 @@
 package kr.or.test;
 
 class NotExistIDException extends Exception {
-//아이디가 존재하지 않을때 예외사항 처리 내용	
+	//아이디가 존재하지 않을때 예외사항 처리 내용
 	public NotExistIDException() {}
 	public NotExistIDException(String message) {
 		super(message);
@@ -14,22 +14,37 @@ class WrongPasswordException extends Exception {
 		super(message);
 	}
 }
+
 public class LoginTryCatch {
 
 	public static void main(String[] args) {
-	try {
-	login("white","12345");
-	login("blue","54321");
+		try {
+			login("white","12345");//로그인1
+		} catch (NotExistIDException e) {
+			e.printStackTrace();
+		} catch (WrongPasswordException e) {
+			e.printStackTrace();
+		}
+		try {
+			login("blue","54321");//로그인2
+		} catch (NotExistIDException e) {
+			e.printStackTrace();
+		} catch (WrongPasswordException e) {
+			e.printStackTrace();
+		}
 	}
-	public static void login(String id, String password)
-	//id가 'blue'가 아니라면 NotEXistIDException 발생시킴
-	if(!id.equals("blue")) {
-		throw new NotExistIDException("아이디가 존재하지 않습니다");
-	}
-	//password가"12345"가 아니라면WrongPasswordException 방생시킴
-	if(!password.equals("12345")) {
-		throw new WrongPasswordException("패스워드가 존재하지 않습니다");
+	
+	public static void login(String id, String password) throws NotExistIDException, WrongPasswordException {
+		//id가 'blue'가 아니라면 NotExistIDException 발생시킴
+		if(!id.equals("blue")) {
+			//강제 오류예외발생 - NotExistIDException
+			throw new NotExistIDException("아이디가 존재하지 않습니다.");
+		}
+		//password가 "12345"가 아니라면 WrongPasswordException 발생시킴
+		if(!password.equals("12345")) {
+			//강제 오류예외발생 - NotExistIDException
+			throw new WrongPasswordException("패스워드가 일치하지 않습니다.");
+		}
 	}
 
 }
-
